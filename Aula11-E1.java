3d1fc298ed8c557d1f37cd5c9a3811dc666613ef
Gladistone <gladistone@hotmail.com>
2016-08-30 15:25:24 -0300

    Exercicios aula 11

---
+++ Aula11-E1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacambalhota;
+
+/**
+ *
+ * @author gladistone
+ */
+public class ProgramaCambalhota {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cc = 0;
+        while (cc<4) {
+            System.out.println("Cambalhota " + cc);
+            cc++;
+        }
+        System.out.println("Tontiei!!!");
+    }
+    
+}
+
